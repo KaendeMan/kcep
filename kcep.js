9cfea9a4c9a09a4cd9720ca0250f6638d9d295d9
KaendeMan <142502968+KaendeMan@users.noreply.github.com>
2023-08-27 02:21:40 +0300

    Update kcep.js

--- kcep.js
+++ kcep.js
@@ -1,23 +1,7 @@
-const navigation = document.querySelector('nav');
-const menuButton = document.querySelector('.menu-button');
-const menuList = document.querySelector('.menu-list');
+// Declare variables
+const projectTitle = "KCEP Educational Computer";
+const userManual = "User Manual for KCEP Educational Computer";
 
-menuButton.addEventListener('click', () => {
-    menuList.classList.toggle('show');
-});
-
-menuList.addEventListener('click', () => {
-    menuList.classList.remove('show');
-});
-
-navigation.addEventListener('click', (event) => {
-    if (event.target.tagName === 'A') {
-        event.preventDefault();
-        const targetId = event.target.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-    }
-});
 // DOM Elements
 const navigation = document.querySelector('nav');
 const menuButton = document.querySelector('.menu-button');
