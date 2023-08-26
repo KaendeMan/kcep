d4d6ef4083587b8235f030b87eaf1f4f8457e601
KaendeMan <142502968+KaendeMan@users.noreply.github.com>
2023-08-27 02:10:25 +0300

    Update kcep.js

--- kcep.js
+++ kcep.js
@@ -18,3 +18,39 @@ navigation.addEventListener('click', (event) => {
         targetSection.scrollIntoView({ behavior: 'smooth' });
     }
 });
+// DOM Elements
+const navigation = document.querySelector('nav');
+const menuButton = document.querySelector('.menu-button');
+const menuList = document.querySelector('.menu-list');
+
+// Toggle mobile menu
+menuButton.addEventListener('click', () => {
+    try {
+        menuList.classList.toggle('show');
+    } catch (error) {
+        console.error("An error occurred while toggling the menu:", error);
+    }
+});
+
+// Close mobile menu when a menu item is clicked
+menuList.addEventListener('click', () => {
+    try {
+        menuList.classList.remove('show');
+    } catch (error) {
+        console.error("An error occurred while closing the menu:", error);
+    }
+});
+
+// Smooth scroll to sections
+navigation.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+        try {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        } catch (error) {
+            console.error("An error occurred while scrolling to section:", error);
+        }
+    }
+});
